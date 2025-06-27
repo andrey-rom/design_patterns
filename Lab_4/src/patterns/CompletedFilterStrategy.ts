d3828c1931b32715907a4b6e1dfd510e4b87cd9d
groupienoseen <ramanenka.andrei@student.ehu.lt>
2025-06-27 18:44:10 +0200

    code refine

--- Lab_4/src/patterns/CompletedFilterStrategy.ts
+++
@@ -1,8 +0,0 @@
-import type { FilterStrategy } from "./FilterStrategy";
-import type { Task } from "../models/Task";
-
-export class CompletedFilterStrategy implements FilterStrategy {
-    filter(tasks: Task[]): Task[] {
-        return tasks.filter(task => task.status === 'Completed');
-    }
-}
--- Lab_4/src/patterns/command/ResetAllCommand.ts
+++
@@ -1,8 +0,0 @@
-import type { Command } from "./Command";
-import { TaskManager } from "../../services/TaskManager";
-
-export class ResetAllCommand implements Command {
-    execute(): void {
-        TaskManager.getInstance().resetAllTasks();
-    }
-} 
